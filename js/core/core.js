0b22d6603de92a85726d4f84c20ca5183cf3ff99
carlosfpj <cafer89@gmail.com>
2022-01-09 17:56:58 -0500

    logic implemented, still upgrades to do

--- js/core/core.js
+++ js/core/core.js
@@ -1,4 +1,8 @@
 let valueDisplay = []
+let firstValue, secondValue, operador
+let waitSecondValue = false
+let firstStringValue = ""
+let secondStringValue = ""
 
 export default function init() {
   let $buttons = []
@@ -9,16 +13,72 @@ export default function init() {
 }
 
 function handleButtonClick(event) {
-  let valueClicked = 0
-  valueClicked = event.target.value
-  valueDisplay.push(valueClicked)
-  valueClicked = 0
-  //fix the array in order to display it, commas appearing
-  showOnDisplay(valueDisplay);
+  if(event.currentTarget.className === "number" || event.currentTarget.className === "decimal") {
+    if(!waitSecondValue) {
+      firstNumber(event)
+      saveFirstValue(event, firstStringValue)
+    } else {
+      saveSecondValue(event)
+    }
+  } else if (event.currentTarget.className === "operator") {
+    waitSecondValue = true
+    operador = event.currentTarget.value
+    showOnDisplay(operador)
+      console.log("oprimiste el operador: " + event.currentTarget.value + " de tipo " + typeof(operador));
+  } else if (event.currentTarget.className === "equal") {
+      console.log("oprimiste el operador igual");
+      const result = performOperation()
+      showOnDisplay(result)
+  } else if (event.currentTarget.className === "all-clear") {
+      console.log("oprimiste all clear");
+      allClear()
+  }
+}
+
+function allClear() {
+  waitSecondValue = false
+  firstStringValue = ""
+  secondStringValue = ""
+  operador = ""
+  firstValue = 0
+  secondValue = 0
+  showOnDisplay(0)
+}
+
+function firstNumber(numberClicked) {
+  firstStringValue = firstStringValue + numberClicked.target.value
+  return firstStringValue
+}
+
+function saveFirstValue(event, stringValue) {
+  firstValue = Number(stringValue)
+  console.log("el primer número es: " + firstValue + " de tipo " + typeof(firstValue))
+  showOnDisplay(firstValue)
+  return firstValue
+}
+
+function saveSecondValue(numberClicked) {
+  secondStringValue = secondStringValue + numberClicked.target.value
+  secondValue = Number(secondStringValue)
+  showOnDisplay(secondValue)
+  console.log("el segundo número es: " + secondValue + " de tipo " + typeof(secondValue))
+  return secondValue
 }
 
 function showOnDisplay(value) {
   const $display = document.querySelector(".display")
-  console.log($display);
   $display.textContent = value
+}
+
+function performOperation() {
+  switch (operador) {
+    case "+":
+      return firstValue + secondValue
+    case "-":
+      return firstValue - secondValue
+    case "*":
+      return firstValue * secondValue
+    case "/":
+      return firstValue / secondValue
+  }
 }
